aa2005648eedd13b99f84819dc6b23c2f8116cc4
Sarah Salvatore <sarah.h.salvatore@gmail.com>
2023-07-18 22:14:25 -0400

    Add progress bar and questions functionality.

--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="questions-ctr">
     <div class="progress">
-      <div class="bar"></div>
-      <div class="status">1 out of 3 questions answered</div>
+      <div
+        class="bar"
+        :style="{ width: `${(questionsAnswered / questions.length) * 100}%` }"
+      ></div>
+      <div class="status">
+        {{ questionsAnswered }} out of {{ questions.length }} questions answered
+      </div>
     </div>
     <div
       class="single-question"
-      v-for="question in questions"
+      v-for="(question, questionIndex) in questions"
+      v-show="questionsAnswered === questionIndex"
       :key="question.q"
     >
       <div class="question">{{ question.q }}</div>
@@ -14,6 +20,7 @@
         class="answers"
         v-for="answer in question.answers"
         :key="answer.text"
+        @click.prevent="selectedAnswer(answer.is_correct)"
       >
         <div class="answer">{{ answer.text }}</div>
       </div>
@@ -24,6 +31,12 @@
 <script>
 export default {
   name: "Questions",
-  props: ["questions"],
+  props: ["questions", "questionsAnswered"],
+  emits: ["question-answered"],
+  methods: {
+    selectedAnswer(is_correct) {
+      this.$emit("question-answered", is_correct);
+    },
+  },
 };
 </script>
